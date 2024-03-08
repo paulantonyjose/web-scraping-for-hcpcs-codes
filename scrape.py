b9d5c4c453b684e25b979169d9c909bda3da1ef1
Paul Antony Jose <paulantonyjose@gmail.com>
2024-03-08 18:58:31 +0530

    Scrape

--- scrape.py
+++ scrape.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 import chromedriver_autoinstaller
-chromedriver_autoinstaller.install()
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
-timeout_duation = 60
 
+
+chromedriver_autoinstaller.install()
 
 def write_to_csv(all_information=None):
     df = pd.DataFrame(all_information)
@@ -18,16 +18,15 @@ def extract_data():
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
+
     driver = webdriver.Chrome(options=chrome_options)
 
-    # Open the main website
-
     driver.get('https://www.hcpcsdata.com/Codes')
+
     main_body = WebDriverWait(driver, 300).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, 'tbody'))
     )
 
-    # Get all the rows in the table body
     all_information =[]
 
     first_page_rows = driver.execute_script("""
@@ -75,9 +74,10 @@ def extract_data():
 
                 new_data['shortDescription'] = short_description
                 del new_data['secondaryLink'], new_data['tertiaryLink']
-            all_information.append(new_data)
+                all_information.append(new_data)
     driver.quit()
     return all_information
+
 if __name__=="__main__":
     all_information = extract_data()
     write_to_csv(all_information=all_information)
